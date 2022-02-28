b18b8fc12895f023b31d05ac8e666ce49b554d9b
EdgenDy <edgengavino@yahoo.com>
2022-02-28 15:46:27 +0800

    Add files via upload

--- src/org/jsengine/v8/internal/tracing/TraceEventHelper.java
+++ src/org/jsengine/v8/internal/tracing/TraceEventHelper.java
@@ -1,1 +1,10 @@
+package org.jsengine.v8.internal.tracing;
 
+import org.jsengine.v8.TracingController;
+import org.jsengine.v8.internal.V8;
+
+public class TraceEventHelper {
+	public static TracingController getTracingController() {
+		return V8.getCurrentPlatform().getTracingController();
+	}
+}
