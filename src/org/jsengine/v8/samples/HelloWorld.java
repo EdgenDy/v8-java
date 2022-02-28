752145b917a940bda6aec78c7e54b3a1a82eec22
EdgenDy <edgengavino@yahoo.com>
2022-02-28 15:50:05 +0800

    Add files via upload

--- src/org/jsengine/v8/samples/HelloWorld.java
+++ src/org/jsengine/v8/samples/HelloWorld.java
@@ -1,1 +1,15 @@
+package org.jsengine.v8.samples;
 
+import org.jsengine.v8.Platform;
+import org.jsengine.v8.V8;
+import org.jsengine.v8.Internal;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Platform platform = Platform.newDefaultPlatform();
+        V8.initializePlatform(platform);
+        System.out.println(platform);
+        System.out.println(Internal.flags.length);
+        System.out.println(Internal.num_flags);
+    }
+}
