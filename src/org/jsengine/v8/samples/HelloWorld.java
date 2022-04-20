1f1b4e360914f9bc57caf4a160824faf0ade61cf
EdgenDy <edgengavino@yahoo.com>
2022-04-20 09:22:07 +0800

    Add files via upload

--- src/org/jsengine/v8/samples/HelloWorld.java
+++ src/org/jsengine/v8/samples/HelloWorld.java
@@ -1,16 +1,19 @@
 package org.jsengine.v8.samples;
 
-import org.jsengine.v8.Platform;
 import org.jsengine.v8.V8;
+import org.jsengine.v8.Isolate;
+import org.jsengine.v8.Platform;
 import org.jsengine.v8.Internal;
+import org.jsengine.v8.ArrayBuffer;
 
 public class HelloWorld {
     public static void main(String[] args) {
         Platform platform = Platform.newDefaultPlatform();
         V8.initializePlatform(platform);
         V8.initialize();
-        //System.out.println(platform);
-        //System.out.println(Internal.flags.length);
-        //System.out.println(Internal.num_flags);
+        
+        Isolate.CreateParams create_params = new Isolate.CreateParams();
+        create_params.array_buffer_allocator = ArrayBuffer.Allocator.newDefaultAllocator();
+        Isolate isolate = Isolate.newInstance(create_params);
     }
 }
